67002976153fba1df51368d23941bffdd1de426a
yarinhazon <60269304+yarinhazon@users.noreply.github.com>
2020-01-24 21:11:38 +0100

    Add files via upload

---
+++ Drill.py
@@ -0,0 +1,10 @@
+ 
+
+paramter1 = input("User name: ") 
+paramter2 = input("\nyour age: ") 
+
+
+
+print("Hi, Your user name is {0}'\n' Your age is {1} ." .format(paramter1, paramter2)) 
+
+paramter3 = input("\ntest: ") 
